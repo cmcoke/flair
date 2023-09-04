b96f0e86c8050dddcee70fd7376aac309b4cdd76
cmcoke <minoru.cm@gmail.com>
2023-09-04 11:06:03 -0500

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import "@/styles/globals.css"
+import { Metadata } from "next"
+
+import { siteConfig } from "@/config/site"
+import { fontSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import { Providers } from "@/components/providers"
+import { SiteBlob } from "@/components/site-blob"
+import { SiteFooter } from "@/components/site-footer"
+import { SiteHeader } from "@/components/site-header"
+
+// sets the title, description of the website using the site.ts file located in the config folder
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico", // gets the favicon.ico file from the public folder
+  },
+}
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
+      </html>
+    </>
+  )
+}
