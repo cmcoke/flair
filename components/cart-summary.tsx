fb76d05304508f39e88468d34d0327933aea93b5
cmcoke <minoru.cm@gmail.com>
2023-09-05 11:37:47 -0500

    integrated stripe into project

--- components/cart-summary.tsx
+++ components/cart-summary.tsx
@@ -7,12 +7,32 @@ import { formatCurrencyString, useShoppingCart } from "use-shopping-cart"
 import { Button } from "@/components/ui/button"
 
 export function CartSummary() {
-  const { formattedTotalPrice, totalPrice, cartDetails, cartCount } =
-    useShoppingCart()
+  const {
+    formattedTotalPrice,
+    totalPrice,
+    cartDetails,
+    cartCount,
+    redirectToCheckout,
+  } = useShoppingCart()
+  const [isLoading, setLoading] = useState(false)
+  const isDisabled = isLoading || cartCount! === 0
   const shippingAmount = cartCount! > 0 ? 500 : 0
   const totalAmount = totalPrice! + shippingAmount
 
-  function onCheckout() {}
+  async function onCheckout(event: MouseEvent) {
+    event.preventDefault()
+    setLoading(true)
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      body: JSON.stringify(cartDetails),
+    })
+    const data = await response.json()
+    const result = await redirectToCheckout(data.id)
+    if (result?.error) {
+      console.error(result)
+    }
+    setLoading(false)
+  }
 
   return (
     <section
@@ -45,9 +65,13 @@ export function CartSummary() {
       </dl>
 
       <div className="mt-6">
-        <Button className="w-full">
-          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          Loading...
+        <Button
+          onClick={(event) => onCheckout(event)}
+          className="w-full"
+          disabled={isDisabled}
+        >
+          {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          {isLoading ? "Loading" : "Checkout"}
         </Button>
       </div>
     </section>
